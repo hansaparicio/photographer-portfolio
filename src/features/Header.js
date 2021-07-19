2f999a57d9c40f89bd25203fdf26f557df074ed7
hans aparicio <hans.moreno@enverus.com>
2021-07-18 22:11:54 -0300

    add alt prop to img

--- src/features/Header.js
+++ src/features/Header.js
@@ -2,7 +2,7 @@ import logo from "../logo.svg";
 
 export const Header = () => (<>
   <div className="header">
-    <img className="header-logo" src={logo}/>
+    <img className="header-logo" src={logo} alt="logo"/>
     <span className="header-title">Mayank Patel</span>
   </div>
   <hr className="header-separator"/>
